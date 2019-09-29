ecd673d0952e494f6d797d4ab8f7b9d0440dfac3
JamesYang <>
2019-09-29 10:11:45 +0800

    jjjj

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from booking.views import hello_world
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
+    # path('',views.index,name='index'),
+    path('', hello_world),
     
 
 ]
--- softway_sys/settings.py
+++ softway_sys/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'softway_sys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\', '/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
