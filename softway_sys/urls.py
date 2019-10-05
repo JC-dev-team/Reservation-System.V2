a80205212c4d081e017ea7233495ea1dfc0f919b
Cheyu Wu <cheyu1220@gmail.com>
2019-10-05 23:53:37 +0800

    no message

--- softway_sys/urls.py
+++ softway_sys/urls.py
@@ -17,5 +17,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('booking/',include('booking.urls')),
-    path('',include('booking.urls'))
+    # path('',include('booking.urls'))
 ]
