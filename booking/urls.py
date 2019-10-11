42f1b22175f8f8e1eabb7326ceca3f52025e24ad
JamesYang <>
2019-10-11 17:32:08 +0800

    no message

--- booking/urls.py
+++ booking/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     # re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     #checking system
-    path('check_reservation/',views.check_reservation, name='check_reservation'),
+    # path('check_reservation/',views.check_reservation, name='check_reservation'),
     
     # Test Views deploy delete 
     path('test001/',views.testView),
