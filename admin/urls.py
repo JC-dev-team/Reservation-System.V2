29c71079c6aa549657f7735901fdb352e90a2ddd
Cheyu Wu <cheyu1220@gmail.com>
2019-10-18 02:55:54 +0800

    no message

--- admin/urls.py
+++ admin/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
     path('checkreservation/',views.staff_check_reservation_page),
     # /softwayliving/reservation/
     path('reservation/',views.staff_reservation_page),
-    
-
 
     ## Ajax api
     # /softwayliving/staff_check/
