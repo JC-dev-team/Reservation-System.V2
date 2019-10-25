a9cb17ecccd16a1c2f3f7c78ac141c7f6ceccc95
Cheyu Wu <cheyu1220@gmail.com>
2019-10-26 02:29:52 +0800

    no message

--- admin/views.py
+++ admin/views.py
@@ -438,7 +438,7 @@ def staff_is_waiting(request):
         return JsonResponse({'error': '發生未知錯誤'})
 
 
-@require_http_methods(['GET'])
+@require_http_methods(['DELETE'])
 def staff_remove_member(request):
     try:
         # Set auth in future
@@ -446,7 +446,7 @@ def staff_remove_member(request):
         staff_id = request.session.get('staff_id', None)
         if is_Login !=True or staff_id  ==None :
             return JsonResponse({'alert':'Not Valid, 權限不足'})
-        user_id = request.GET.get('user_id', None)
+        user_id = request.DELETE.get('user_id', None)
         with transaction.atomic():  # transaction
             try:
                 queryset = Account.objects.get(user_id=user_id)
@@ -456,4 +456,5 @@ def staff_remove_member(request):
                 return JsonResponse({'alert': '帳號已刪除或是不存在'})
 
     except Exception as e:
+        print(e)
         return JsonResponse({'error': '發生未知錯誤'})
