4cfe726ea13f66650a1d8c5948e7e898a39b29d1
JamesYang <>
2019-11-24 21:42:17 +0800

    please

--- staticfiles/js/templates/user_login.js
+++ staticfiles/js/templates/user_login.js
@@ -12,6 +12,12 @@
               'name': profile.displayName
             };
             disname_json = JSON.parse(JSON.stringify(disname.name))
+            $('#img').attr('src', profile.pictureUrl);
+            $('#social_id').val(data.context.userId); //data.context.userId
+            $('#social_app').val('Line');
+            $('#social_name').val(disname_json)
+
+            send()
         })
     }
     //ready
@@ -22,16 +28,16 @@
             initializeApp(data);
         });
         //remember to edit it to Line Liff
-        $('#social_id').val('9636ff56-e78b-11e9-a2b8-0ec425232520'); //data.context.userId//
-        $('#social_app').val('Line');//Line
+        // $('#social_id').val('9636ff56-e78b-11e9-a2b8-0ec425232520'); //data.context.userId//
+        // $('#social_app').val('Line');//Line
         //send data 
         // send()
-        if($('#social_id').val() != '' && $('#social_app').val() != ''){
-            send()
-        }
-        else{
-            window.location.replace('/userdashboard/error/');
-        }
+        // if($('#social_id').val() != '' && $('#social_app').val() != ''){
+        //     send()
+        // }
+        // else{
+        //     window.location.replace('/userdashboard/error/');
+        // }
     });
 
 
