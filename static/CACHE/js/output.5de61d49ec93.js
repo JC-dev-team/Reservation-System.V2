d1d1f6041d460027b93e5472831942ac612d4963
JamesYang <>
2019-11-24 20:10:36 +0800

    james new

---
+++ static/CACHE/js/output.5de61d49ec93.js
@@ -0,0 +1,2 @@
+;function admin_reservation(){Notiflix.Loading.Hourglass('讀取中....');window.location.href='/softwayliving/reservation/'}
+function admin_check(){Notiflix.Loading.Hourglass('讀取中....');window.location.href='/softwayliving/checkreservation/'}
---
+++ static/CACHE/js/output.8e600c475c7a.js
@@ -0,0 +1,3 @@
+;function closewindow(){if(''=='admin'){window.location.href='/softwayliving/login/'}else{liff.closeWindow()}}
+$(function(){if(''=='admin'){document.getElementById('close_button').innerText='返回管理者介面'}
+if(''!='0'){document.getElementById('information').innerText="訂位資訊 (候補 "+''+")"}})
